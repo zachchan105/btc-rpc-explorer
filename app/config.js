54eaec6a94cb56007b6fc17a3a5820c39d19b8d1
jheleniak <john.heleniak@gmail.com>
2019-12-17 23:56:16 -0500

    Update config.js

--- app/config.js
+++ app/config.js
@@ -177,7 +177,7 @@ module.exports = {
 		{name:"RPC Browser", url:"/rpc-browser", desc:"Browse the RPC functionality of this node. See docs and execute commands.", fontawesome:"fas fa-book"},
 		{name:"RPC Terminal", url:"/rpc-terminal", desc:"Directly execute RPCs against this node.", fontawesome:"fas fa-terminal"},
 
-		{name:(coins[currentCoin].name + " Fun"), url:"/fun", desc:"See fun/interesting historical blockchain data.", fontawesome:"fas fa-certificate"}
+		//{name:(coins[currentCoin].name + " Fun"), url:"/fun", desc:"See fun/interesting historical blockchain data.", fontawesome:"fas fa-certificate"}
 	],
 
 	donations:{
